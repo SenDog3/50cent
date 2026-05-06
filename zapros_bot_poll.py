566fed7fd2704d39fdff78d9b77f4ef057c3ec8a
SenDog <vszotov3@gmail.com>
2026-05-06 15:52:42 +0300

    add file users with ids in my_folder

--- zapros_bot_poll.py
+++ zapros_bot_poll.py
@@ -13,7 +13,7 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 ID_MAIN = os.getenv('ID_MAIN')  # использовать для служебных сообщений
 GROUP_ID = os.getenv('group_id_main_small') # group_id_main_small  = "-1003425228475" 
 
-with open('/app/users.txt', 'r') as file:
+with open('/my_folder/users.txt', 'r') as file:
     user_ids = [line.strip() for line in file]
 
 if not BOT_TOKEN:
