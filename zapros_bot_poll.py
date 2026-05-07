000b8f2cbd4187944a3418529a427d5b5b1a9045
SenDog <vszotov3@gmail.com>
2026-05-07 09:17:28 +0300

    add file users with ids in app/my_folder и проверка id

--- zapros_bot_poll.py
+++ zapros_bot_poll.py
@@ -194,6 +194,8 @@ def main():
                         # Обрабатываем только приватные чаты
                         if chat_type == 'private' and chat_id in user_ids:
                             handle_message(message)
+                        else:     
+                            bot.send_message(chat_id, f"У вас нет доступа к боту. ваш id {chat_id} и {user_ids} и {type(user_ids)} и {type(chat_id)}")
 
                     # Обрабатываем ответы на опросы
                     elif 'poll_answer' in update:
